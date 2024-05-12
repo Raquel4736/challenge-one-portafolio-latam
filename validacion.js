6b442ca063076f729285889ed430bcdbea2d291b
Raquel Ruiz Diaz <raquel.r.diaz04@gmail.com>
2024-05-12 07:57:19 -0300

    portafolio

--- validacion.js
+++ validacion.js
@@ -1,1 +1,13 @@
 //Haz tú validación en javascript acá
+
+function confirmacion() {
+  var respuesta=confirm("¿Desea enviar este formulario?");
+  if (respuesta ==true) {
+    respuesta=confirm("El mensaje ha sido enviado");
+    
+    return true
+  }else{
+    return false
+  }
+  
+}
